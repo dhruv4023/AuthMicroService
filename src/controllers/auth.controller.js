994ddf957a70c16b615eb0e78ba8befd21e7cfc8
dhruv <dhruv20345@gmail.com>
2024-02-28 17:23:35 +0530

    username added to token

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -134,7 +134,7 @@ export const loginControl = async (req, res) => {
         return RESPONSE.error(res, 1005, 400);
 
       // Generate a JWT token for the authenticated user
-      const token = generateJWTToken({ data: { userId: user.id, role: user.role }, });
+      const token = generateJWTToken({ data: { userId: user.id, username: user.username, role: user.role } });
 
       // Hide the password in the user object before sending the response
       user.password = undefined;
